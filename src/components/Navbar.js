289563ef49e51ab0cff0ab2d61bb30fc4b0726ec
harshittsaini <harshitsaini61@gmail.com>
2024-04-05 15:23:55 +0530

    url updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div >
       <header class="text-gray-600 body-font">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <Link to="/movie-space" class="flex title-font font-medium items-center text-gray-600 mb-4 md:mb-0">
+          <Link to="/" class="flex title-font font-medium items-center text-gray-600 mb-4 md:mb-0">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
